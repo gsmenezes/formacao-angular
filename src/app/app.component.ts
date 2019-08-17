5c99883e9dc831fdcc06ad67dfa60e796850f397
Gilda Menezes <gildamnxcosta@gmail.com>
2019-08-17 19:02:34 -0300

    adequa o template e organiza o codigo em modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {   
+export class AppComponent {
+
+  photos = [
+    {
+      url: 'https://br.seaicons.com/wp-content/uploads/2017/03/blue-heart-icon.png',
+      description: 'Coração azul',
+    },
+    {
+      url: 'https://i2.wp.com/multarte.com.br/wp-content/uploads/2018/10/coracao-png.png?fit=2400%2C1706&ssl=1',
+      description: 'Coração',
+    }
+  ];
+
 }
--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -8,4 +8,4 @@ import { Component, Input } from '@angular/core';
 export class PhotoComponent {
     @Input() description = '';
     @Input() url = '';
-}
+  }
