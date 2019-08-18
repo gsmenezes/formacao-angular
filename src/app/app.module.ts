eb2d867f9d536e3b972d2c42d0c15f0a65c3dd73
Gilda Menezes <gildamnxcosta@gmail.com>
2019-08-18 01:25:43 -0300

    single page applications e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { PhotosModule } from './photos/photos.module';
-import { HttpClientModule } from '@angular/common/http';
-
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     PhotosModule,
-    HttpClientModule,
+    ErrorsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-const API = 'http://localhost:3000';
+import { Photo } from './photo';
+
+const API = 'http://localhost:3000/flavio/photos';
 
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
@@ -11,7 +13,6 @@ export class PhotoService {
     listFromUser(userName: string) {
 
         return this.http
-            .get<Object[]>('http://localhost:3000/flavio/photos');
-
+            .get<Photo[]>(API + '/' + userName + '/photos');
     }
 }
